124903b1928b2c8a6500fc7f5f24cd44a35daa57
İbo Bey <117900246+ibobey@users.noreply.github.com>
2023-07-10 14:23:19 +0300

    main.py simplified

--- main.py
+++ main.py
@@ -5,11 +5,10 @@ from concurrent.futures import ThreadPoolExecutor
 from DAL.POSTGRES_MANAGEMENT.PostgresManager import *
 
 
-RAW_URL = "https://www.pse.pl/getcsv/-/export/csv/EN_CENY_NIEZB_RB/data/"
-THREAD = 6
+def update_download_records():
 
-
-if __name__ == "__main__":
+    RAW_URL = "https://www.pse.pl/getcsv/-/export/csv/EN_CENY_NIEZB_RB/data/"
+    THREAD = 6
 
     last_record_date_from_database = PostgresManager().get_last_record()
     start_date_for_scrapping = last_record_date_from_database
@@ -21,6 +20,13 @@ if __name__ == "__main__":
                 edited_data = DataManager(raw_data=result).get_edited_data()
                 if edited_data is not None:
                     pgm.insert_into(data=edited_data)
+
+
+
+if __name__ == "__main__":
+    update_download_records()
+
+
 
 
 
