e8bd8a4c929142658882d0399b76cd99b5aea707
İbo Bey <117900246+ibobey@users.noreply.github.com>
2023-07-10 10:10:49 +0300

    Queries.py inserted new query

--- DAL/POSTGRES_MANAGEMENT/PACKS/Queries.py
+++ DAL/POSTGRES_MANAGEMENT/PACKS/Queries.py
@@ -7,3 +7,12 @@ CREATE_TABLE_IF_NOT_EXISTS = ""
 INSERT_INTO = ""
 
 GET_LAST_RECORD = ""
+
+CREATE_DATABASE = """
+    CREATE DATABASE poland_market_data
+        WITH
+        OWNER = postgres
+        ENCODING = 'UTF8'
+        CONNECTION LIMIT = -1
+        IS_TEMPLATE = False;
+"""
