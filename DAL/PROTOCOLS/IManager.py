7b43a3baf1e303bfb96826f9113c4170f90f13be
İbo Bey <117900246+ibobey@users.noreply.github.com>
2023-07-10 10:09:54 +0300

    IManager.py Protocol Created

---
+++ DAL/PROTOCOLS/IManager.py
@@ -0,0 +1,34 @@
+from typing import Protocol, NoReturn, TypeVar, List
+
+
+class IManager(Protocol):
+
+    connection: TypeVar
+    cursor: TypeVar
+
+    def __connect_database(self) -> NoReturn:
+        ...
+
+    def __close_database_connection(self) -> NoReturn:
+        ...
+
+    def __set_credentials(self) -> NoReturn:
+        ...
+
+    def __create_database(self) -> bool:
+        ...
+
+    def __create_table_if_not_exists(self) -> bool:
+        ...
+
+    def commit(self) -> NoReturn:
+        ...
+
+    def query_database(self) -> bool:
+        ...
+
+    def insert_into(self) -> bool:
+        ...
+
+    def fetch_all(self) -> List:
+        ...
